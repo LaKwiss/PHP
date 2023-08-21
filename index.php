2cb44b873f029957a7cc08e1637bc61b44aa472b
Yann Bälli <y.baelli05@gmail.com>
2023-08-21 11:29:12 +0200

    Ex Tableau (7)

--- index.php
+++ index.php
@@ -4,49 +4,21 @@
 
 <body>
 <?php
-$a = date("n");
-$mois = "";
-switch($a){
-    case 1:
-        $mois = "janvier";
-        break;
-    case 2:
-        $mois = "Février";
-        break;
-    case 3:
-        $mois = "Mars";
-        break;
-    case 4:
-        $mois = "Avril";
-        break;
-    case 5:
-        $mois = "Mai";
-        break;
-    case 6:
-        $mois = "Juin";
-        break;
-    case 7:
-        $mois = "Juillet";
-        break;
-    case 8:
-        $mois = "Août";
-        break;
-    case 9:
-        $mois = "Septembre";
-        break;
-    case 10:
-        $mois = "Octobre";
-        break;
-    case 11:
-        $mois = "Novembre";
-        break;
-    case 12:
-        $mois = "Décembre";
-        break;
+
+echo "\n<table>";
+
+for ($i=1; $i <=12; $i++) {
+    echo "\n\t<tr>";
+    for ($j=1;$j<=12;$j++) {
+        $t = $i * $j;
+        echo "\n\n\t<td>$t</td>";
+    }
+
+
+
+    echo "</tr>";
 }
-
-
-echo "<p>mois = $mois </p>"
+echo "\n</table>\n";
 
 ?>
 </body>
