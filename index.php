f09d647b5ad284ea9d1ad8d3998c4eb1b89ff3d9
Yann Bälli <y.baelli05@gmail.com>
2023-08-21 09:30:54 +0200

    Ex 1, 2, 3

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Exemple1</title>
+</head>
+<body>
+<?php
+
+$nom = "Bälli";
+$prenom = "Yann";
+$proffession = "Développeur";
+
+echo "<table border=\"2\">";
+echo "<tr>";
+echo "<td>Nom</td>";
+echo "<td>Prénom</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>Lerdorf</td>";
+echo "<td>Rasmus</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>Computer</td>";
+echo "<td>Jules</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>$nom</td>";
+echo "<td>$prenom</td>";
+echo "<td>$proffession</td>";
+echo "</tr>";
+echo "</table>";
+?>
+</body>
+</html>
