eed871becaaaf0d40e244cbf75e72c5cfe09fdc1
Yann Bälli <y.baelli05@gmail.com>
2023-08-21 10:38:09 +0200

    Ex 6

--- index.php
+++ index.php
@@ -4,14 +4,50 @@
 
 <body>
 <?php
-
-$seconde = date("s");
-
-echo $seconde;
-
-if ($seconde < 30){
-    echo "<p>Salut</p>";
+$a = date("n");
+$mois = "";
+switch($a){
+    case 1:
+        $mois = "janvier";
+        break;
+    case 2:
+        $mois = "Février";
+        break;
+    case 3:
+        $mois = "Mars";
+        break;
+    case 4:
+        $mois = "Avril";
+        break;
+    case 5:
+        $mois = "Mai";
+        break;
+    case 6:
+        $mois = "Juin";
+        break;
+    case 7:
+        $mois = "Juillet";
+        break;
+    case 8:
+        $mois = "Août";
+        break;
+    case 9:
+        $mois = "Septembre";
+        break;
+    case 10:
+        $mois = "Octobre";
+        break;
+    case 11:
+        $mois = "Novembre";
+        break;
+    case 12:
+        $mois = "Décembre";
+        break;
 }
+
+
+echo "<p>mois = $mois </p>"
+
 ?>
 </body>
 </html>
