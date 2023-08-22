0ba889c84d1bfe01fa3022dd8975f4995d5866b0
Yann Bälli <y.baelli05@gmail.com>
2023-08-22 08:53:26 +0200

    Tableau 2.2
    Multiplication

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
 
 <body>
 <?php
-
 $tab = array(1 => 7,14,21,28,35,42,49,56,63,70,77,84);
 
-echo "<pre>";
-print_r($tab);
-echo "</pre>";
+
+for ($i =1; $i <= 12; $i++){
+    echo "$i * 7 = $tab[$i]";
+    echo "<br>";
+}
 ?>
 </body>
 </html>
