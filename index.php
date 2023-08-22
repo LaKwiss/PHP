0bb8e418d06b75b6c15c147ce03806cd4ef37dca
Yann Bälli <y.baelli05@gmail.com>
2023-08-22 08:05:12 +0200

    Jour de la semaine (array)

--- index.php
+++ index.php
@@ -5,20 +5,38 @@
 <body>
 <?php
 
-echo "\n<table>";
+$jour = date("w");
+$j = "";
+$tableau = array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
 
-for ($i=1; $i <=12; $i++) {
-    echo "\n\t<tr>";
-    for ($j=1;$j<=12;$j++) {
-        $t = $i * $j;
-        echo "\n\n\t<td>$t</td>";
-    }
-
-
-
-    echo "</tr>";
+switch ($jour-1){
+    case 0:
+        $j = $tableau[0];
+        break;
+    case 1:
+        $j = $tableau[1];
+        break;
+    case 2:
+        $j = $tableau[2];
+        break;
+    case 3:
+        $j = $tableau[3];
+        break;
+    case 4:
+        $j = $tableau[4];
+        break;
+    case 5:
+        $j = $tableau[5];
+        break;
+    case 6:
+        $j = $tableau[6];
+        break;
+    case 7:
+        $j = $tableau[7];
+        break;
 }
-echo "\n</table>\n";
+
+echo $j;
 
 ?>
 </body>
